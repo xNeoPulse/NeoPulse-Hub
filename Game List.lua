731feffd743e2bad8ff31283c7df2be8ec4a4927
xNeoPulse <charlesgorre2001@gmail.com>
2026-05-04 11:03:10 +0800

    Update Game List.lua

--- Game List.lua
+++ Game List.lua
@@ -1,4 +1,6 @@
 local Games = {
+    [2239430935] = "https://raw.githubusercontent.com/xNeoPulse/Game-Scripts/main/Blair.lua"; -- Blair
+    [9091133975] = "https://raw.githubusercontent.com/xNeoPulse/Game-Scripts/main/CatchAndTame.lua"; -- Catch and Tame
     [8944906740] = "https://raw.githubusercontent.com/xNeoPulse/Game-Scripts/main/DiveDown.lua"; -- Dive Down
     [7703614594] = "https://raw.githubusercontent.com/xNeoPulse/Game-Scripts/main/GardenTowerDefense.lua"; -- Garden Tower Defense
 }
